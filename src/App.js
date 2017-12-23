5922f54a1bb80d4afdb7f929c349c4df010d8ee7
Liz Verde <>
2017-12-23 23:29:14 +0000

    Adding in the football pitch background, the favicon, the title (to be changed) and the logo - spinning like the react one.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                <img src={ logo } className="App-logo" alt="logo" />
+                <h1>Ftbll Tm Rndmisr </h1>
+                <h2>Pick your teams</h2>
+                </header>
+      
+            </div>
+        );
+    }
 }
 
 export default App;
