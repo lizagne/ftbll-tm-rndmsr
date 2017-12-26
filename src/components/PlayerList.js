c2ce056919044036fcb0ecd587b9bba53cc2accb
Liz Verde <>
2017-12-26 18:59:44 +0000

    Work has rolled back so I can list out names again, will commit this then go over some redux tutorials to see where I was going wrong before

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,16 +1,17 @@
-import React from 'react';
-// import xBtn from "../images/xbutton.svg";
+import React, { Component } from 'react';
+import xBtn from "../images/xbutton.svg";
 import XButton from "./XButton";
 
-const PlayerList = ({ playerList }) => (
-	<div>
-		{ playerList.map((player, i) => 
-			<XButton 
-				key={ i } 
-				id={ i }
-			>{ player }</XButton>
-		)}
-	</div>
-);
+class PlayerList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          	<li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default PlayerList;
--- src/components/XButton.js
+++ src/components/XButton.js
@@ -1,11 +1,11 @@
-import React from "react";
-import xBtn from "../images/xbutton.svg";
+// import React from "react";
+// import xBtn from "../images/xbutton.svg";
 
-export default ({ children, onClick, id }) => (
-	<div className="main-player animated fadeInRight">
-		<button 
-			onClick={ () => onClick(id) }>
-			<img src={ xBtn } alt="delete" /></button>
-		<p>{ children }</p>	
-	</div>
-);
+// export default ({ children, onClick, id }) => (
+// 	<div className="main-player animated fadeInRight">
+// 		<button 
+// 			onClick={ () => onClick(id) }>
+// 			<img src={ xBtn } alt="delete" /></button>
+// 		<p>{ children }</p>	
+// 	</div>
+// );
