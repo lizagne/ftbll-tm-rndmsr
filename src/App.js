c58fe221a151bb57b79968e1f4ba428d02b72d71
Liz Verde <>
2017-12-27 09:12:34 +0000

    Restructuring everything, deleting old components that weren't working and creating a new MultiInput component that will create an array of names, also changed some styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import Input from './components/Input';
 import './App.css';
+import MultiInput from "./components/MultiInput";
+//containers to put in later
+// import InputForm from './containers/InputForm';
+// import Options from './containers/Options';
+// import TeamOutput from '/containers/TeamOutput';
 
 class App extends Component {
     render() {
         return (
             <div  className="App">
                 <Header />
-                <Input />
+                <MultiInput />
+                {/* to put in later
+                <InputForm />
+                <Options />
+                <TeamOutput />
+            	*/}
+
             </div>
         );
     }
