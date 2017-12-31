6d60fddffe7523c7678e67e2ceadc9981a0f170e
Liz Verde <>
2017-12-31 12:31:21 +0000

    Deleting older redux files to start afresh

--- src/data/InitialState.js
+++
@@ -1,9 +0,0 @@
-//using Immutable JS to keep everything immutable, importing in List and Map to set up the initial state.
-import { Map, List } from 'immutable';
-
-const InitialState = Map({ 
-	input: "",
-	players: List([]),
-});
-
-export default InitialState;
--- src/data/constants.js
+++
@@ -1,4 +0,0 @@
-export const UPDATE_INPUT = "UPDATE_INPUT";
-export const ADD_NAME = "ADD_NAME";
-export const DELETE_NAME = "DELETE_NAME";
-
