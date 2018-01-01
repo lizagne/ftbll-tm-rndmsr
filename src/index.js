dd8cedd0eb137eaf974150bb9e5199cd40f6e018
Liz Verde <>
2018-01-01 11:52:48 +0000

    Updating the index to combine the reducers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
-import App from './App';
 
+import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import Reducer from './data/Reducer';
+//import Reducers to then combine together:
+import playersReducer from './reducers/Reducer';
+import teamReducer from './reducers/teamReducer';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
-const store = createStore(Reducer);
+import thunk from 'redux-thunk';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const enhancer = compose(
+    applyMiddleware(thunk),
+    devTools
+);
+
+const store = createStore(combineReducers({ players: playersReducer, teams: teamReducer }), enhancer);
+window.store = store;
 
 ReactDOM.render(
-	<Provider store={ store }>
-		<App />
-	</Provider>,
-	 document.getElementById('root')
+    <Provider store={ store } >
+    	<App />
+    </Provider>, document.getElementById('root')
 );
 
