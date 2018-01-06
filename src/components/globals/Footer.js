e9b96f0f68593d82c8d556ff20b6f9d0517b7b2b
Liz Verde <>
2018-01-06 22:38:37 +0000

    Putting links out to code and website into the sticky footer

--- src/components/globals/Footer.js
+++ src/components/globals/Footer.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Footer = () => (
 	<div>
         <footer>
-            <p><a href="http://www.lizagne.uk" target="_blank" rel="noopener noreferrer">Liz Verde</a></p>
+            <a href="http://www.lizagne.uk" className="footer" target="_blank" rel="noopener noreferrer"><i className="fa fa-external-link-square" aria-hidden="true"></i> Liz Verde</a>
 
+            <a href="https://github.com/lizagne/ftbll-tm-rndmsr" className="footer" target="_blank" rel="noopener noreferrer"><i className="fa fa-external-link-square" aria-hidden="true"></i> View Code on GitHub</a>
+
+            <a href="https://developme.training/fellowship/" className="footer" target="_blank" rel="noopener noreferrer"><i className="fa fa-external-link-square" aria-hidden="true"></i> A final task set by DevelopMe_</a>
         </footer>
     </div>
 );
